41168ecd4c994d0a547b9652fb1b9211062241b8
Gopi <gopikrishna.gummadidala@gmail.com>
2021-04-26 21:29:43 +0200

    latest code changes

---
+++ projects/polling/src/lib/polling.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-polling',
+  template: `
+    <p>
+      polling works!
+    </p>
+  `,
+  styles: []
+})
+export class PollingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ projects/polling/src/lib/polling.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { PollingComponent } from './polling.component';
+
+
+
+@NgModule({
+  declarations: [PollingComponent],
+  imports: [
+  ],
+  exports: [PollingComponent]
+})
+export class PollingModule { }
