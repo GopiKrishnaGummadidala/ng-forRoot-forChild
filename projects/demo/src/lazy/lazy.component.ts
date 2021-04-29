a4f0fc692e8ac85a2bba858b525c17781bec662e
Gopi <gopikrishna.gummadidala@gmail.com>
2021-04-29 21:55:07 +0200

    latest code changes

--- projects/demo/src/lazy/lazy.component.ts
+++ projects/demo/src/lazy/lazy.component.ts
@@ -3,9 +3,9 @@ import { PollingService } from "polling/polling";
 
 @Component({
   selector: "app-lazy",
-  template: `<p>
+  template: `<span>
     Lazy Polling count times {{ pollingService.polling$ | async }}
-  </p>`,
+  </span>`,
 })
 export class LazyComponent implements OnInit {
   constructor(private pollingService: PollingService) {}
