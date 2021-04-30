14651f6dedcbe52b08a519201e748740911c24cf
Gopi <gopikrishna.gummadidala@gmail.com>
2021-04-30 20:55:13 +0200

    latest code changes

--- projects/polling/src/lib/polling.module.ts
+++ projects/polling/src/lib/polling.module.ts
@@ -1,3 +1,4 @@
+import { INTERVAL, PollingConfig } from "./polling.config";
 import { PollingService } from "./polling.service";
 import { ModuleWithProviders, NgModule } from "@angular/core";
 import { PollingComponent } from "./polling.component";
@@ -12,6 +13,18 @@ export class PollingModule {
     return {
       ngModule: PollingModule,
       providers: [PollingService],
+    };
+  }
+  static forChild(config: PollingConfig): ModuleWithProviders<PollingModule> {
+    return {
+      ngModule: PollingModule,
+      providers: [
+        PollingService,
+        {
+          provide: INTERVAL,
+          useValue: config.interval || 2000,
+        },
+      ],
     };
   }
 }
