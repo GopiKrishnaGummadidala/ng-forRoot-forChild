59a26cef1fc254a375bfec66772d51356de5c47a
Gopi <gopikrishna.gummadidala@gmail.com>
2021-04-30 21:03:51 +0200

    latest code changes

--- projects/demo/src/lazy/lazy.module.ts
+++ projects/demo/src/lazy/lazy.module.ts
@@ -7,6 +7,10 @@ import { LazyRoutingModule } from "./lazy.routing.module";
 
 @NgModule({
   declarations: [LazyComponent],
-  imports: [CommonModule, LazyRoutingModule, PollingModule],
+  imports: [
+    CommonModule,
+    LazyRoutingModule,
+    PollingModule.forChild({ interval: 3000 }),
+  ],
 })
 export class LazyModule {}
--- projects/polling/src/lib/polling.service.ts
+++ projects/polling/src/lib/polling.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from "@angular/core";
+import { Inject, Injectable, Optional } from "@angular/core";
 import { timer } from "rxjs";
 import { shareReplay } from "rxjs/operators";
+import { INTERVAL } from "./polling.config";
 
 @Injectable()
 export class PollingService {
-  public polling$ = timer(0, 1000).pipe(shareReplay());
-  constructor() {}
+  public polling$ = timer(0, this.interval || 1000).pipe(shareReplay());
+  constructor(@Optional() @Inject(INTERVAL) private interval: number) {}
 }
